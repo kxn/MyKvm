d886a5b68dea3bdcdbac21374d394377f3186fe9
kangxn <kangxn@gmail.com>
2026-07-31 15:31:06 +0800

    feat: add RFB pixel format support (#2)

--- crates/ipkvm-rfb/src/lib.rs
+++ crates/ipkvm-rfb/src/lib.rs
@@ -4,6 +4,7 @@ mod framebuffer;
 mod protocol;
 
 pub use framebuffer::{BgraFrameView, RfbFramebufferError, RfbRectangle, RfbSize};
+pub use protocol::pixel_format::{RfbColorChannel, RfbPixelFormat, RfbPixelFormatError};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct RfbServerConfig {
--- crates/ipkvm-rfb/src/protocol/mod.rs
+++ crates/ipkvm-rfb/src/protocol/mod.rs
@@ -1,1 +1,2 @@
+pub(crate) mod pixel_format;
 pub(crate) mod wire;
